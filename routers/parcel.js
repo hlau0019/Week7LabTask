12948826d331c99d01a67d53bff8366c7319b136
Cons Lau <conslau@dyn-118-138-71-61.its.monash.edu.au>
2022-09-09 10:29:25 +1000

    Extra task

--- routers/parcel.js
+++ routers/parcel.js
@@ -34,11 +34,14 @@ module.exports = {
     //Extra task
     updateById: function(req, res){
 
-        Parcel.findOneAndUpdate({_id: req.body._id}, (Parcel.weight)*10, function(err,parcel){
-            if (err) return res.status(400).json(err);
-            if (!parcel) return res.status(404).json();
-
-            res.json(parcel);
+        Parcel.find({_id: req.body._id},function(err,parcel){
+            Parcel.findOneAndUpdate(Parcel.weight*10,function(err,parcel){
+                if (err) return res.status(400).json(err);
+                if (!parcel) return res.status(404).json();
+    
+                res.json(parcel);
+            })
         })
+
     }
 }
