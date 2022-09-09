fa25da2b658dbeb6d5574b1dc1628ad86f56fa7b
Cons Lau <conslau@dyn-118-138-71-61.its.monash.edu.au>
2022-09-09 10:24:41 +1000

    Extra task

--- routers/parcel.js
+++ routers/parcel.js
@@ -34,7 +34,7 @@ module.exports = {
     //Extra task
     updateById: function(req, res){
 
-        Parcel.findOneAndUpdate({_id: req.body._id}, Parcel.weight*10, function(err,parcel){
+        Parcel.findOneAndUpdate({_id: req.body._id}, (Parcel.weight)*10, function(err,parcel){
             if (err) return res.status(400).json(err);
             if (!parcel) return res.status(404).json();
 
